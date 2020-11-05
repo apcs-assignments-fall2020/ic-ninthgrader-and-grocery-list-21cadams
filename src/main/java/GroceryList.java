474be018af0a31b40c3cf7dcbedbbd0c2f77f891
21cadams <21cadams@athenian.org>
2020-11-05 07:22:15 +0000

    Grocery List

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,39 @@
 public class GroceryList {
+    private String[] arr;
+
+    public GroceryList() {
+        // our grocery list will have a max size of 10
+        this.arr = new String[10]; 
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
     
-}
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            int left = arr.length - (i+1);
+            if (arr[i] == item) {
+                for (int x=1; x <= left; x++)
+                    arr[i+(x-1)] = arr[i+x];
+                break;    
+            }
+        }
+    }
+
+    public String toString(){
+        String all = "Grocery List: ";
+        for (int i = 0; i < arr.length; i++) {
+            all = all + arr[i];
+            if (i != arr.length-1){
+                all = all + "; ";
+            }
+        }
+        return all;
+    }
+}   
--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,19 @@
-public class NinthGrader {
+public class NinthGrader extends Student {
     
+    public NinthGrader(String a, String b) {
+        super(a, b, 2024);
+    }
+
+    public NinthGrader(){
+        super("John",  "Smith", 2024);
+    }
+
+    public void doSomething() {
+        System.out.println("I am studdying physics.");
+    }
+
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.getClassYear() +"(Ninth Grader)";
+        return str;
+    }
 }
